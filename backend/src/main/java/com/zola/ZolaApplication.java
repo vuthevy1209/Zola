741f4ca80b2b9d82037978ecacee12a2e418a4ad
Vũ Thế Vỹ <vuthevy12092004@gmail.com>
2026-01-28 09:26:18 +0700

    init: backend source

---
+++ backend/src/main/java/com/zola/ZolaApplication.java
@@ -0,0 +1,13 @@
+package com.zola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZolaApplication.class, args);
+	}
+
+}
