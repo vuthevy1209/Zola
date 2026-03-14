c1360b3bad84fb6409716c261990d23566c446bb
Vũ Thế Vỹ <vuthevy12092004@gmail.com>
2026-03-14 09:38:25 +0700

    chore: remove PublicAttributeController and add this logic into AttributeController

--- backend/src/main/java/com/zola/controllers/AttributeController.java
+++ backend/src/main/java/com/zola/controllers/AttributeController.java
@@ -17,23 +17,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/attributes")
+@RequestMapping("/attributes")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@PreAuthorize("hasAuthority('ADMIN')")
 public class AttributeController {
 
     SizeService sizeService;
     ColorService colorService;
 
     // --- SIZES ---
-    @PostMapping("/sizes")
-    public ApiResponse<SizeResponse> createSize(@RequestBody @Valid SizeRequest request) {
-        return ApiResponse.<SizeResponse>builder()
-                .result(sizeService.createSize(request))
-                .build();
-    }
-
     @GetMapping("/sizes")
     public ApiResponse<List<SizeResponse>> getAllSizes() {
         return ApiResponse.<List<SizeResponse>>builder()
@@ -41,7 +33,16 @@ public class AttributeController {
                 .build();
     }
 
+    @PostMapping("/sizes")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ApiResponse<SizeResponse> createSize(@RequestBody @Valid SizeRequest request) {
+        return ApiResponse.<SizeResponse>builder()
+                .result(sizeService.createSize(request))
+                .build();
+    }
+
     @PutMapping("/sizes/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ApiResponse<SizeResponse> updateSize(@PathVariable Long id, @RequestBody @Valid SizeRequest request) {
         return ApiResponse.<SizeResponse>builder()
                 .result(sizeService.updateSize(id, request))
@@ -49,6 +50,7 @@ public class AttributeController {
     }
 
     @DeleteMapping("/sizes/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ApiResponse<String> deleteSize(@PathVariable Long id) {
         sizeService.deleteSize(id);
         return ApiResponse.<String>builder()
@@ -57,13 +59,6 @@ public class AttributeController {
     }
 
     // --- COLORS ---
-    @PostMapping("/colors")
-    public ApiResponse<ColorResponse> createColor(@RequestBody @Valid ColorRequest request) {
-        return ApiResponse.<ColorResponse>builder()
-                .result(colorService.createColor(request))
-                .build();
-    }
-
     @GetMapping("/colors")
     public ApiResponse<List<ColorResponse>> getAllColors() {
         return ApiResponse.<List<ColorResponse>>builder()
@@ -71,7 +66,16 @@ public class AttributeController {
                 .build();
     }
 
+    @PostMapping("/colors")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ApiResponse<ColorResponse> createColor(@RequestBody @Valid ColorRequest request) {
+        return ApiResponse.<ColorResponse>builder()
+                .result(colorService.createColor(request))
+                .build();
+    }
+
     @PutMapping("/colors/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ApiResponse<ColorResponse> updateColor(@PathVariable Long id, @RequestBody @Valid ColorRequest request) {
         return ApiResponse.<ColorResponse>builder()
                 .result(colorService.updateColor(id, request))
@@ -79,6 +83,7 @@ public class AttributeController {
     }
 
     @DeleteMapping("/colors/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ApiResponse<String> deleteColor(@PathVariable Long id) {
         colorService.deleteColor(id);
         return ApiResponse.<String>builder()
